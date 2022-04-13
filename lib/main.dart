ca2c6dbcf60aa4a9e6317f3367738ed330166c73
Makhmudov Alijon <98184267+alijonmakhmudov@users.noreply.github.com>
2022-04-14 00:49:38 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:udevs_do_do/home_screens.dart';
+import 'package:get/get.dart';
+import 'package:hive_flutter/adapters.dart';
+void main() async{
+  await Hive.initFlutter();
+ await Hive.openBox('task');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  ResponsiveSizer(
+        builder: (context, orientation, screenType) {
+          return const HomeScreens();
+        },
+      ),
+    );
+  }
+}
+
+
